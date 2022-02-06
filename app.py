92b54e0b35393a5b118846372d97d0091402e82f
Iwanttobeatuna <zadchin@gmail.com>
2022-02-06 14:32:46 -0500

    umap label added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
@@ -15,6 +16,14 @@ df = pd.read_csv('umap_appended.csv')
 
 ## tsne csv
 df_2 = pd.read_csv('tsne_appended.csv')
+
+## Split into dataframes based on labels (umap)
+df_umap_1 = df.loc[df['labels'] == 0]
+df_umap_2 = df.loc[df['labels'] == 1]
+df_umap_3 = df.loc[df['labels'] == 2]
+df_umap_1_describe = df_umap_1.describe()
+df_umap_2_describe = df_umap_2.describe()
+df_umap_3_describe = df_umap_3.describe()
 
 ## umap initial plot
 fig_2d = px.scatter(
@@ -30,10 +39,196 @@ fig_tsne = px.scatter(
     hover_data=['icustay_id']
 )
 
+## Label analysis function
+def plot_features(features, df, y_label):
+    mean_df = df.loc[['mean'], df.columns.to_series().str.contains(features)]
+    median_df = df.loc[['50%'], df.columns.to_series().str.contains(features)]
+    mean_Transpose = mean_df.transpose()
+    median_Transpose = median_df.transpose()
+    ## merge the two dataframes
+    mean_median_Transpose = pd.merge(mean_Transpose, median_Transpose, left_index=True, right_index=True)
+    mean_median_Transpose.columns = ['mean', 'median']
+    ## plot the dataframe
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=mean_median_Transpose.index, y=mean_median_Transpose['mean'], name='mean', mode='lines'))
+    fig.add_trace(go.Scatter(x=mean_median_Transpose.index, y=mean_median_Transpose['median'], name='median', mode='lines'))
+    fig.update_layout(xaxis_title='Time', yaxis_title=y_label)
+    return fig
+
+
 ## Bootstrap template
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP]) 
 
 app.layout = dbc.Container([
+
+    html.Br(),
+    
+    dbc.Row([html.H1(children='UMAP label analysis', style={'textAlign': 'center'})], style={'padding': '10px'}),
+
+    dbc.Row
+    ([
+         dbc.Col([
+            
+            dbc.Row([
+
+                html.H5('Label 0', style={'textAlign': 'center'}),
+
+                html.Div([dcc.Graph(figure = plot_features('heart_rate', df_umap_1_describe, 'Heart Rate'))], 
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('respiratory_rate', df_umap_1_describe, 'Respiratory Rate'))], 
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('hematocrit', df_umap_1_describe, 'hematocrit'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('fraction_inspired_oxygen', df_umap_1_describe, 'Fi02'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('creatinine', df_umap_1_describe, 'creatinine'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('mean_blood_pressure', df_umap_1_describe, 'mbp'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('sodium', df_umap_1_describe, 'sodium'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+        ],width=4, sm=4, md=4, lg=4),
+        
+        
+        dbc.Col([
+            
+            dbc.Row([
+
+                html.H5('Label 1', style={'textAlign': 'center'}),
+
+                html.Div([dcc.Graph(figure = plot_features('heart_rate', df_umap_2_describe, 'Heart Rate'))], 
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('respiratory_rate', df_umap_2_describe, 'Respiratory Rate'))], 
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('hematocrit', df_umap_2_describe, 'hematocrit'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('fraction_inspired_oxygen', df_umap_2_describe, 'Fi02'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('creatinine', df_umap_2_describe, 'creatinine'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('mean_blood_pressure', df_umap_2_describe, 'mbp'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('sodium', df_umap_2_describe, 'sodium'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+        ],width=4, sm=4, md=4, lg=4),
+
+        dbc.Col([
+            
+            dbc.Row([
+
+                html.H5('Label 2', style={'textAlign': 'center'}),
+
+                html.Div([dcc.Graph(figure = plot_features('heart_rate', df_umap_3_describe, 'Heart Rate'))], 
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('respiratory_rate', df_umap_3_describe, 'Respiratory Rate'))], 
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('hematocrit', df_umap_3_describe, 'hematocrit'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('fraction_inspired_oxygen', df_umap_3_describe, 'Fi02'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('creatinine', df_umap_3_describe, 'creatinine'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('mean_blood_pressure', df_umap_3_describe, 'mbp'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+
+            dbc.Row([
+
+                
+                html.Div([dcc.Graph(figure = plot_features('sodium', df_umap_3_describe, 'sodium'))],
+                style={'width': '100%', 'display': 'inline-block'})
+            ]),
+        ],width=4, sm=4, md=4, lg=4),
+    ]),
+
+    html.Hr(),
 
     ## UMAP
     html.Br(),
